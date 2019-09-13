b128014ac8c3b6a0bd0c77a52fffb381bfba6bb1
root <adamschunk@gmail.com>
2019-09-13 20:26:24 +0000

    added things to test

--- test.py
+++ test.py
@@ -1,5 +1,24 @@
 import sc2reader as reader
 import os
 
-replay_dir = 'test_replays/engagemetnts'
-replays = reader.load_replays(os.path.join(replay_dir))
+replay_dir = 'test_replays/engagements'
+replay = reader.load_replay(os.path.join(replay_dir,"test.SC2Replay"))
+
+
+event_names = set([event.name for event in replay.events])
+events_of_type = {name: [] for name in event_names}
+for event in replay.events:
+    events_of_type[event.name].append(event)
+
+unit_born_events = events_of_type["UnitBornEvent"]
+for ube in unit_born_events:
+    print("Player {} created {}".format(ube.control_pid,
+        ube.unit_type_name))
+
+
+unit_died_events = events_of_type["UnitDiedEvent"]
+for udiede in unit_died_events:
+        print("Unit id {} was killed by player {} using {}".format(udiede.unit_id_index,
+            udiede.killing_player_id, udiede.killing_unit_index))
+
+
